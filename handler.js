7870009cf1ed31192908fb5c00b505b6cbfc89c4
Nathan Snyder <snides111@gmail.com>
2022-11-24 21:38:02 -0600

    try to fix cors

--- handler.js
+++ handler.js
@@ -3,35 +3,18 @@ require("dotenv").config();
 const serverless = require("serverless-http");
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
 const spotifyRoutes = require("./routes/spotify-routes");
 const contactRoutes = require("./routes/contact-routes");
 
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser());
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-  })
-);
 
 app.use("/api/spotify", spotifyRoutes);
 
 app.use("/api/contact", contactRoutes);
 
-app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from root!",
-  });
-});
-
-app.get("/hello", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from path!",
-  });
-});
+app.use(cors());
 
 app.use((req, res, next) => {
   return res.status(404).json({
